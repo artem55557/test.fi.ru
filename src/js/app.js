2e80395ee6b6871125af624fe70af8f1f208b02b
Artem Netesov <netyosov.a@yandex.ru>
2020-10-02 16:12:48 +0700

    Сверстал контентую часть

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Custom JS
+  const navBlock = document.querySelector('.nav-block')
+  const navLink = navBlock.querySelectorAll('.nav-block__link')
+  const progress = navBlock.querySelector('.nav-block__progress')
+
+  navLink.forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault()
+      navLink.forEach( i => {
+        i.parentElement.classList.remove('active')
+      })
+      item.parentElement.classList.add('active')
+      questionsProgress()
+    })
+  })
+
+  function questionsProgress() {
+    const activeEl = document.querySelector('.nav-block__item.active')
+    const ofset = 9
+    const DOMRectNavBlok = navBlock.getBoundingClientRect()
+    const DOMRectAvtiveEl = activeEl.getBoundingClientRect()
+    const widthProgress = DOMRectAvtiveEl.x - DOMRectNavBlok.x + DOMRectAvtiveEl.width - ofset
+    const widthPercentProgress = widthProgress * 100 / DOMRectNavBlok.width
+
+    progress.style.width = `${widthPercentProgress}%`
+  }
+
+  questionsProgress()
 
 });
